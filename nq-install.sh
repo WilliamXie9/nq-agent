e6ffe58103d85c3053dd79f3f60d5e49f1478224
WilliamAnda <williamxie@anda.network>
2020-04-02 11:04:56 +0800

    Update nq-install.sh

--- nq-install.sh
+++ nq-install.sh
@@ -16,6 +16,9 @@
 
 # Set environment
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export TESTID="s4ef3w4g54aaa"
+readonly TESTID
+
 
 # Prepare output
 echo -e "|\n|   NodeQuery Installer\n|   ===================\n|"
